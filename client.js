083e0403200f81f02ea12a180b426c5a95c280e5
James <jcorr851@gmail.com>
2023-06-09 17:06:40 -0400

    Some aesthetic changes to the webpage

--- client.js
+++ client.js
@@ -1,12 +1,18 @@
 window.addEventListener("DOMContentLoaded", () => {
+  const divider = document.createElement("div")
+  divider.setAttribute("style", "justify-content: center; margin: 5%; border: 0.3em solid black; background-color:#ffffee")
+  document.body.appendChild(divider)
+
+
   const messages = document.createElement("ul");
-  document.body.appendChild(messages);
+  //messages.setAttribute()
+  divider.appendChild(messages);
 
 
   const websocket = new WebSocket("ws://localhost:5678/");
   websocket.onmessage = ({ data }) => {
     const message = document.createElement("li");
-	message.setAttribute("style", "font-family:'Courier New'; white-space: pre-line")
+	message.setAttribute("style", "font-family:'Yu Gothic', 'Courier New'; font-size: 30px; white-space: pre-line; list-style-type: none;")
     const content = document.createTextNode(data);
     message.appendChild(content);
     messages.prepend(message);
