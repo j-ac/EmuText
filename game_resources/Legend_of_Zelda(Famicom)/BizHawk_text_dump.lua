84d12c14963bbdbff9c66f5d4b9980bc475fd56c
James <jcorr851@gmail.com>
2023-06-11 20:59:48 -0400

    Added support for Legend Of Zelda (Famicom)

---
+++ game_resources/Legend_of_Zelda(Famicom)/BizHawk_text_dump.lua
@@ -0,0 +1,29 @@
+-- Dumps text from Legend of Zelda for Famicom (Full title: Zelda no Densestu 1 - The Hyrule Fantasy)
+-- For use with the BizHawk emulator
+
+local DUMP_KEY = "G"				-- Key which triggers a text dump
+memory.usememorydomain("CIRAM (nametables)")	-- Defines which section of memory the memory API accesses indexed from 0x0
+local POSITION_IN_CIRAM = 0x0090
+local TEXT_LENGTH_BYTES = 0x260
+
+while true
+do
+	local keys = input.get()
+	if keys[DUMP_KEY] == true
+	then
+		mem = memory.read_bytes_as_array(POSITION_IN_CIRAM, TEXT_LENGTH_BYTES)
+
+		-- ===============
+		-- === FILE IO ===
+		-- ===============
+		local f = io.open("dump.txt", "w")
+		io.output(f)
+		for i = 1, #mem
+		do
+			io.write(tostring(mem[i]) .. ", ")
+		end
+		io.close()
+
+	end
+	emu.frameadvance() -- otherwise the script hogs CPU and starves the game
+end
