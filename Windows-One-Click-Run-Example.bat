2a8faaa966b7e9bef8146ec126a6014250a31cf6
jcorr <jcorr851@gmail.com>
2023-06-25 01:12:18 -0400

    Batch script uses %USERPROFILE% variable
    Previously used a hardcoded value,
    this will use the correct value for any
    user by default

--- Windows-One-Click-Run-Example.bat
+++ Windows-One-Click-Run-Example.bat
@@ -2,19 +2,19 @@
 :: Replace each path with the path on your own windows system.
 
 :: Launch BizHawk and immediately load the ROM and the script
-set executable="C:\Users\j-ac\Documents\EmuText\BizHawk-2.9.1-win-x64\EmuHawk.exe"
-set rom="C:\Users\j-ac\Documents\EmuText\BizHawk-2.9.1-win-x64\ROM\Pocket Monsters - Blue Version (J) [S].sgb"
-set script="C:\Users\j-ac\Documents\EmuText\game_resources\Pokemon_Blue_JP\BizHawk_text_dump.lua"
+set executable="%USERPROFILE%\Documents\EmuText\BizHawk-2.9.1-win-x64\EmuHawk.exe"
+set rom="%USERPROFILE%\Documents\EmuText\BizHawk-2.9.1-win-x64\ROM\Pocket Monsters - Blue Version (J) [S].sgb"
+set script="%USERPROFILE%\Documents\EmuText\game_resources\Pokemon_Blue_JP\BizHawk_text_dump.lua"
 
 start "" %executable% --lua=%script% %rom%
 
-:: Launch the server minimized, using the appropriate resource pack 
-set python="C:\Users\j-ac\AppData\Local\Microsoft\WindowsApps\python.exe"
-set server="C:\Users\j-ac\Documents\EmuText\server.py"
-set resources="C:/Users/j-ac/Documents/EmuText/game_resources/Pokemon_Blue_JP/"
+:: Launch the server minimized, using the appropriate resource folder 
+set python="%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\python.exe"
+set server="%USERPROFILE%\Documents\EmuText\server.py"
+set resources="%USERPROFILE%/Documents/EmuText/game_resources/Pokemon_Blue_JP/"
 
 start /min "" %python% %server% %resources% %*
 
 :: Launch the local web client
-set client="C:\Users\j-ac\Documents\EmuText\EmuText.html"
+set client="%USERPROFILE%\Documents\EmuText\EmuText.html"
 %client%
