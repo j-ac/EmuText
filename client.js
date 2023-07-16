483e3fd79ada030e745a112fec9da3750e7c0594
James <jcorr851@gmail.com>
2023-07-16 15:49:19 -0400

    Replaced lots of Javascript with HTML

--- client.js
+++ client.js
@@ -1,40 +1,4 @@
 window.addEventListener("DOMContentLoaded", () => {
-  // Create style
-  var style = document.createElement('style');
-  style.innerHTML = ".main-font { font-family:'Yu Gothic', 'Courier New'; font-size: 30px; white-space: pre-line; list-style-type: none;}";
-  document.head.appendChild(style);
-
-  // ====================
-  // === FONT BUTTONS ===
-  // ====================
-
-  var decreaseButton = document.createElement('button');
-  decreaseButton.textContent = 'Decrease Font Size';
-  decreaseButton.onclick = function() {
-    var styleBlock = document.querySelector("style");
-	var mainFontRule = styleBlock.sheet.cssRules[0];
-	currentFontSize = parseInt(mainFontRule.style.fontSize);
-	mainFontRule.style.fontSize = currentFontSize - 2 + 'px';
-  };
-  
-  var increaseButton = document.createElement('button');
-  increaseButton.textContent = 'Increase Font Size';
-  increaseButton.onclick = function() {
-    var styleBlock = document.querySelector("style");
-	var mainFontRule = styleBlock.sheet.cssRules[0];
-	currentFontSize = parseInt(mainFontRule.style.fontSize);
-	mainFontRule.style.fontSize = currentFontSize + 2 + 'px';
-  };
-
-  //Button Container
-  var container = document.createElement("div");
-  container.setAttribute("style", "display: flex; justify-content: center;")
-  document.body.appendChild(container)
-
-  container.appendChild(increaseButton)
-  container.appendChild(decreaseButton)  
-  //=========================
-
   // Divider that holds the text
   const divider = document.createElement("div")
   divider.setAttribute("style", "justify-content: center; margin: 5%; border: 0.3em solid black; background-color:#ffffee")
@@ -47,7 +11,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const websocket = new WebSocket("ws://localhost:5678/");
   websocket.onmessage = ({ data }) => {
     const message = document.createElement("li");
-	message.classList.add('main-font');
+	//message.classList.add('main-font');
     const content = document.createTextNode(data);
     message.appendChild(content);
     messages.prepend(message);
