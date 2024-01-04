20ce29e3f471e840e16e7ab14e260e1da20dd12c
James <jcorr851@gmail.com>
2024-01-03 23:16:57 -0500

    Gameboy_text_dump.lua functions as a library

--- lib/gameboy_tile_dump.lua
+++ lib/gameboy_tile_dump.lua
@@ -5,28 +5,20 @@
 
 -- Once font data is stored in JSON, and meanings are mapped between data in VRAM and corresponding UTF-8 characters, EmuLiveText's Python server can interpret dynamically changing data.
 
-json = dofile "./json.lua"
+json = require ("json")
 memory.usememorydomain("VRAM")
 output_file = "tiles.json"
 
+-- Constants
 local DUMP_KEY = "J"
 BYTE_SIZE = 8
 SPRITE_SIZE = 2 * BYTE_SIZE --bytes
-START_TILE = 320 -- Tile number
-NUM_SPRITES = 64 -- number of sprites in contiguous memory from the starting tile.
-
 VRAM_START = 0x8000
 
-DUMP_START = SPRITE_SIZE * START_TILE
-DUMP_END = DUMP_START + NUM_SPRITES * SPRITE_SIZE
-
-
---brkpnt = true
---while brkpnt do
---	emu.frameadvance()
---end
+dumper = {} -- Function library
 
 -- Loads in a json file and converts it into a dictionary using "data" as its key
+-- This allows data to persist through multiple uses of the script
 function decode_json()
 	ret = {}
 
@@ -42,10 +34,8 @@ function decode_json()
 
 	return ret
 end
-sprite_data = decode_json()
 
-
-
+-- Returns the bit at position `index` in a `number`
 function extract(number, index)
 	local mask = 2^(index-1)
 	--if mask == nil or mask > 32 or mask < 0 then print(mask) end
@@ -63,8 +53,6 @@ function interpret_line(first_byte, second_byte)
 end
 
 -- Turn a sprite byte array into a string of human-readable UTF-8 characters ▒▓█
-
-num_pics = 0 -- DEBUG
 function interpret_sprite(data)
 	local pic = "\n"
 
@@ -95,39 +83,51 @@ function byte_array_to_string(arr)
 	return ret
 end
 
-REMOVE_GREY_SPRITES = true -- Whether to discard sprites that use ▓▓ or ▒▒. Because in some games they are certainly junk characters.
-while true do
-	for i=DUMP_START, DUMP_END - SPRITE_SIZE, SPRITE_SIZE do -- For each sprite
-		bytes = memory.read_bytes_as_array(i, SPRITE_SIZE)
-		as_string = byte_array_to_string(bytes)
+-- The primary function called externally.
+-- remove_grey_sprites (boolean): Whether to discard sprites that use ▓▓ or ▒▒. In some games they are certainly junk characters.
+-- start_tile (int): first tile that should be included in the dump. BizHawk puts tile numbers in the "GPU Viewer" in the gameboy emulator
+-- num_sprites (int): how many tiles should be included in the dump beginning from start_tile
+dumper.dump_sprites_forever = function(remove_grey_sprites, start_tile, num_sprites)
+	sprite_data = decode_json() -- loads the known sprite data from previous times running the script
 
-		if sprite_data[as_string] == nil then
-			picture = interpret_sprite(bytes)
-			if (REMOVE_GREY_SPRITES == true and string.find(picture, "▓▓") ~= nil or string.find(picture, "▒▒") ~= nil) then goto skip end 
-			sprite_data[as_string] = {picture = interpret_sprite(bytes), character = "", location = string.format("%04X", i + VRAM_START)}
-		end
-		::skip::
-	end
+	dump_start = SPRITE_SIZE * start_tile
+	dump_end = dump_start + num_sprites * SPRITE_SIZE
 
+	while true do
+		for i=dump_start, dump_end - SPRITE_SIZE, SPRITE_SIZE do -- For each sprite
+			bytes = memory.read_bytes_as_array(i, SPRITE_SIZE)
+			as_string = byte_array_to_string(bytes)
 
-	local keys = input.get()
-	if keys[DUMP_KEY] == true then
-		local f = io.open(output_file, "wb")
-		io.output(f)
-
-		for key, val in pairs(sprite_data) do
-			out_str = json.encode({
-			data = key,
-			picture = val.picture,
-			character = val.character,
-			location = val.location})
-			io.write(out_str .. "\n")
+			if sprite_data[as_string] == nil then
+				picture = interpret_sprite(bytes)
+				if (remove_grey_sprites == true and string.find(picture, "▓▓") ~= nil or string.find(picture, "▒▒") ~= nil) then goto skip end 
+				sprite_data[as_string] = {picture = interpret_sprite(bytes), character = "", location = string.format("%04X", i + VRAM_START)}
+			end
+			::skip::
 		end
 
-		io.close()
-		print("Successfully wrote to ".. output_file)
+
+		local keys = input.get()
+		if keys[DUMP_KEY] == true then
+			local f = io.open(output_file, "wb")
+			io.output(f)
+
+			for key, val in pairs(sprite_data) do
+				out_str = json.encode({
+				data = key,
+				picture = val.picture,
+				character = val.character,
+				location = val.location})
+				io.write(out_str .. "\n")
+			end
+
+			io.close()
+			print("Successfully wrote to ".. output_file)
+		end
+
+		emu.frameadvance()
+
 	end
-
-	emu.frameadvance()
-
 end
+
+return dumper
