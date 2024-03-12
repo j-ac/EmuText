e988ebe14868d68dc8d273120763b4e1eb45a237
James <jcorr851@gmail.com>
2024-03-11 23:50:11 -0400

    Improve build script

--- build.bat
+++ build.bat
@@ -2,4 +2,6 @@
 :: Build script for Emu LiveText
 echo Building server executable...
 pyinstaller %cd%\server.py --icon=.\resources\64x64.ico --onefile --name EmuLiveText
+move %CD%\dist\EmuLiveText.exe %CD%
+rmdir dist
 pause
